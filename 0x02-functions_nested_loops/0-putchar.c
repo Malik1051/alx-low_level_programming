545afd38c3ce6f00c422633e9b214decab6a7ec6
Ben Malik <benmaleek271@gmail.com>
2022-12-28 15:13:57 +0000

    _putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -4,18 +4,19 @@
  * main - print _putchar
  *  is written by Ben
  * 
- *Always return - 0
-*/
+ * Always return 0
+ */
 int main(void)
 {
-_putchar('_'); 
-_putchar('p');   
-_putchar('u');
-_putchar('t');
-_putchar('c');
-_putchar('h');
-_putchar('a');
-_putchar('r');
-_putchar('\n');
+    _putchar('_');
+    _putchar('p');
+    _putchar('u');
+    _putchar('t');
+    _putchar('c');
+    _putchar('h');
+    _putchar('a');
+    _putchar('r');
+    _putchar('\n');
+
     return (0);
-} 
+}
